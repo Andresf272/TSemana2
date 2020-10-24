a72f904f5dd7b879dc5e91ddf1509d7b2805fcfe
Andres1984 <andresfaruelo@gmail.com>
2020-10-24 20:10:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tsemana1/ListadoDeMAscotas.java
@@ -0,0 +1,14 @@
+package com.example.tsemana1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoDeMAscotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_de_m_ascotas);
+    }
+}
